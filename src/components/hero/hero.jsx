fc0dec1d094af2ae626b879719890497a027c455
BAA77 <142021188+BAA77@users.noreply.github.com>
2025-06-12 05:23:02 -0700

    hero buttons

--- src/components/hero/hero.jsx
+++ src/components/hero/hero.jsx
@@ -17,7 +17,7 @@ function Hero() {
                         <h1>Custom Construction & Remodeling Services Across New Hampshire.</h1>
                         <p>High-quality craftsmanship from roofing and siding to kitchens, decks, and full remodels. <br /><b>— If you can dream it, we can build it.</b></p>
                         <div className="hero-btn">
-                            <a id='hero-f-btn' className='hero-f-btn' onClick={() => {navigate('/contact')}} href="#">Get Free Quote</a>
+                            <a id='hero-f-btn' className='hero-f-btn' onClick={() => {navigate('/contact')}} href="#">Free Quote</a>
                             <a id='hero-s-btn' className='hero-s-btn' href="#services">See Services</a>
                         </div>
                     </div>
